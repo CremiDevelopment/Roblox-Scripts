a23103f2e973fde10324a38d1a333ed236373450
Cremi <89044831+CremiDevelopment@users.noreply.github.com>
2023-05-18 21:17:11 +0300

    Update GetExecutor.lua

--- GetExecutor.lua
+++ GetExecutor.lua
@@ -5,5 +5,5 @@ print(exec_name)
 local UI = loadstring(game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/twink", true))()
 
 UI.Load("Example UI name")
-UI.Load.TabLibrary.AddPage("Example Page Title", true)
-UI.Load.PageLibrary.AddLabel("Example Label")
+UI.TabLibrary.AddPage("Example Page Title", true)
+UI.PageLibrary.AddLabel("Example Label")
