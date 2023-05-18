8b1cc36ac02dae11b5e6e822f7b8a0ec6d90002b
Cremi <89044831+CremiDevelopment@users.noreply.github.com>
2023-05-18 20:22:06 +0300

    Create GetExecutor.lua

---
+++ GetExecutor.lua
@@ -0,0 +1,8 @@
+local exec_name = (getexecutor or getexecutorname or getidentityexecutor or identifyexecutor or function() return 'Unknown' end)()
+
+if exec_name != 'Unknown' then
+  print('Executor name: ' .. exec_name .. ' ; And copied to clipboard');
+  setclipboard(exec_name);
+else 
+  print('Executor is unknown!');
+end;
