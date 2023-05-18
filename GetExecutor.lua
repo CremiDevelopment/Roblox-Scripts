9a81bed86e8509a7fd38d2c67ee7a1f657ee5d56
Cremi <89044831+CremiDevelopment@users.noreply.github.com>
2023-05-18 20:51:42 +0300

    Update GetExecutor.lua

--- GetExecutor.lua
+++ GetExecutor.lua
@@ -1,9 +1,3 @@
 local exec_name = (getexecutor or getexecutorname or getidentityexecutor or identifyexecutor or function() return 'Unknown' end)()
 
-if exec_name != 'Unknown' then
-  print('Executor name: ' .. exec_name .. ' ; And copied to clipboard');
-  game:GetService("StarterGui"):SetCore("ChatMakeSystemMessage", { Text ='Executor name: ' .. exec_name .. ' ; And copied to clipboard'})
-  setclipboard(exec_name);
-else 
-  print('Executor is unknown!');
-end;
+print(exec_name)
